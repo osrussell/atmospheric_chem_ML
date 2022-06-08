e1afe43503d27acc49cc4d44616c0b268ef4cf25
Olivia Russell <russell.olivia@gmail.com>
2022-06-08 15:48:58 -0700

    Cleaning up site finder

--- find_sites.py
+++ find_sites.py
@@ -19,19 +19,22 @@ class findSites():
 
     def __init__(self):
         """
-        initializes the object with the dataframe you want checked
+        initializes the site finder
         """
-        # TODO: has a dataframe with the site codes!!!
-        # TODO: also clean up this part
-        self.site_codes = []
         self.datafetcher = DataFetcher()
 
     def find_sites(self, param, state, byear, eyear=None):
         """
-        param: String -- name of the parameter we're searching for 
-        state: String -- code for the state
-        bdate: int -- first year we're collecting data for
-        edate: int -- last year we're collecting data for if there is one
+        Finds sites for a certain parameter within the range of byear to eyear
+
+        Parameters:
+            param: String -- name of the parameter we're searching for 
+            state: String -- code for the state
+            bdate: int -- first year we're collecting data for
+            edate: int -- last year we're collecting data for if there is one
+
+        Returns:
+            A dataframe with all the sites with valid data for the time range
         """
 
         #TODO: wind handling!!!
@@ -57,37 +60,43 @@ class findSites():
         df = df.drop_duplicates(subset = "site_number")
         df = df.reset_index()
 
-        # adds a column for true/false for the variable 
-        # TODO: add a way to see the site name
-        df['site_name'] = df['site_number']
         # print(df)
         df[param] = True
 
-        # returns just the site numbers, 
-        # return df[['site_number','open_date', 'close_date', param]]
-        return df[['site_number', 'site_name', param]]
+        return df[['site_number', 'county_code', param]]
     
     def best_sites_state(self, state, byear, eyear=None, other_params=[]):
         """
-        state: String -- state code
-        byear: int -- first year
-        eyear: int -- last year (if there is one)
-        other_params: [String] -- list of parameters we want besides ozone and pm2.5 (names)
+        Finds all sites with potential data for ozone, pm2.5, and any other parameters
+
+        Parameters:
+            state: String -- state code
+            byear: int -- first year
+            eyear: int -- last year (if there is one)
+            other_params: [String] -- list of parameters we want besides ozone and pm2.5 (names)
+
+        Returns:
+            A dataframe with all valid sites. 
+            Every parameter as a 1 (meaning there is information for that time range) or a 0 (no information)
         """
         # first combines list with ozone and PM2.5
         # since we only are doing ozone rn
         dfs = self.find_sites('Ozone', state, byear, eyear)
         params = ['PM2.5 - Local Conditions'] + other_params
+        param_dict = {'Ozone' : 'sum'}
 
         # loops through all the parameters
         for param in params:
             #find_sites('Ozone', '06', 1980, 2020)
             df = self.find_sites(param, state, byear, eyear)
             dfs = pd.concat([dfs, df]).fillna(value=False)
+            param_dict[param] = 'sum'
 
-        dfs = dfs.groupby(dfs['site_number']).sum()
+        mini_func = {'county_code': 'first'}
+        aggregation_functions = {**mini_func, **param_dict}
+        dfs = dfs.groupby(dfs['site_number']).aggregate(aggregation_functions)
+
         all_params = ['Ozone'] + params
         dfs = dfs.sort_values(by=all_params, ascending=False)
-        # reformats the data frame
 
         return dfs
